306bcf971e5bc66afea6dc9531e7c41e45611d0c
Gadik <kkpomsk@gmail.com>
2018-04-03 19:43:49 +0000

    test listDemo init

---
+++ src/test/java/ListLab/ListDemoTest.java
@@ -0,0 +1,40 @@
+package ListLab;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListDemoTest {
+    @Test
+    void firstSymbolEquals() {
+    }
+
+    @Test
+    void sameSurname() {
+    }
+
+    @Test
+    void goOutHuman() {
+    }
+
+    @Test
+    void noCommonWith() {
+    }
+
+    @Test
+    void oldest() {
+    }
+
+    @Test
+    void SNFsortedArray() {
+    }
+
+    @Test
+    void choosens() {
+    }
+
+    @Test
+    void youngetThen18() {
+    }
+
+}
