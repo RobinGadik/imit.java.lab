f7f3820ee7b646c61c647c8a814eef2d82a2c165
Gadik <kkpomsk@gmail.com>
2018-04-03 17:11:37 +0000

    1 done

--- src/main/java/ListLab/ListDemo.java
+++ src/main/java/ListLab/ListDemo.java
@@ -1,5 +1,16 @@
 package ListLab;
 
-public class ListDemo {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ListDemo {
+    public static List<String> firstSymbolEquals(List<String> a,char b){
+        List<String> c = new ArrayList<String>();
+        for (String i: a) {
+            if(i.charAt(0) == b) {
+                c.add(i);
+            }
+        }
+        return c;
+    }
 }
