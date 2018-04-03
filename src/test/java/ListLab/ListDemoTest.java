62c63ae5987578472a05b5ed5992d19919253592
Gadik <kkpomsk@gmail.com>
2018-04-03 19:52:49 +0000

    test add

--- src/test/java/ListLab/ListDemoTest.java
+++ src/test/java/ListLab/ListDemoTest.java
@@ -2,19 +2,36 @@ package ListLab;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ListDemoTest {
     @Test
     void firstSymbolEquals() {
+        ArrayList<String> a = new ArrayList<String>();
+        ArrayList<String> b = new ArrayList<String>();
+        a.add("aaa");
+        b.add("aaa");
+        a.add("cfb");
+        assertEquals(b, ListDemo.firstSymbolEquals(a,'a'));
     }
 
     @Test
     void sameSurname() {
+        ArrayList<Human> a = new ArrayList<>();
+        ArrayList<Human> c = new ArrayList<>();
+        Human b = new Human("1","1","1",13);
+        a.add(new Human("1","1","2",12));
+        c.add(new Human("1","1","2",12));
+        a.add(new Human("1","2","2",2));
+        assertEquals(c, ListDemo.sameSurname(a,b));
     }
 
     @Test
     void goOutHuman() {
+
     }
 
     @Test
