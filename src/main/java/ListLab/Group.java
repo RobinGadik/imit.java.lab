1c256a4f70abaeb4bb88e21f0acffaad4bc42e0b
Gadik <kkpomsk@gmail.com>
2018-04-03 19:00:25 +0000

    Group init

---
+++ src/main/java/ListLab/Group.java
@@ -0,0 +1,28 @@
+package ListLab;
+
+public class Group {
+    int index;
+    int[] data;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public int[] getData() {
+        return data;
+    }
+
+    public void setData(int[] data) {
+        this.data = data;
+    }
+
+    public Group(int index, int... data) {
+
+        this.index = index;
+        this.data = data;
+    }
+}
