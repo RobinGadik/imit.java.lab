fac1bb351f3cc410152266a5f160ae3da9883625
Gadik <kkpomsk@gmail.com>
2018-04-03 13:08:41 +0000

    work

--- src/main/java/MatrixLab/IMatrix.java
+++ src/main/java/MatrixLab/IMatrix.java
@@ -1,5 +1,8 @@
 package MatrixLab;
 
 public interface IMatrix {
+    void set(int str, int clm, double vol);
+    double get(int str,int clm);
+    double determinant();
 
 }
--- src/main/java/MatrixLab/Matrix.java
+++ src/main/java/MatrixLab/Matrix.java
@@ -2,4 +2,28 @@ package MatrixLab;
 
 public class Matrix implements IMatrix {
     public static boolean testing(){return true;}
+
+    double[] m;
+    int size;
+    Matrix(){
+        m = new double[1];
+        size = 1;
+    }
+
+    Matrix(int n){
+        m = new double[n*n];
+        size = n;
+    }
+
+    public void set(int str, int clm, double vol) {
+        m[str*size+clm] = vol;
+    }
+
+    public double get(int str, int clm) {
+        return m[str*size+clm];
+    }
+
+    public double determinant() {
+        return 0;
+    }
 }
