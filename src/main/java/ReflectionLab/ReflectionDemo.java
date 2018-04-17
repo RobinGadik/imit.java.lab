65795be86f62a81f91939b7ab6b1c73b911b528e
Gadik <kkpomsk@gmail.com>
2018-04-17 20:31:31 +0000

    1 2 done

--- src/main/java/ReflectionLab/ReflectionDemo.java
+++ src/main/java/ReflectionLab/ReflectionDemo.java
@@ -1,4 +1,40 @@
 package ReflectionLab;
 
+import ListLab.Human;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReflectionDemo {
+
+    public int humanListCheck(List<Object> a){
+        int res=0;
+        for(Object o:a){
+            if(o.getClass() == Human.class || o.getClass().getSuperclass() == Human.class){
+                res++;
+            }
+        }
+
+        return res;
+    }
+
+    public List<String> methodsList(Object o){
+        List<String> a = new ArrayList<>();
+
+        Method[] met = o.getClass().getMethods();
+
+        for(Method m:met){
+            if(m.getModifiers() == Modifier.PUBLIC){
+                a.add(m.getName());
+            }
+        }
+
+        return a;
+    }
+
+
+
+
 }
